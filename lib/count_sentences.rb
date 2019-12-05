d059a1d5d0689aa05f610287f0629342a9432576
Luana Faria Lane <luanasfff@gmail.com>
2019-12-05 20:15:28 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,7 +3,8 @@ require 'pry'
 class String
 
   def sentence?(string)
-   if self.string.end_with?(".")
+    self.string = string 
+      if  string.end_with?(".")
      return true 
    else 
      false 
