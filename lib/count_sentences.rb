991112c268d484b8723a7eef460a659bc2cad288
Luana Faria Lane <luanasfff@gmail.com>
2019-12-05 20:38:09 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,42 @@ require 'pry'
 class String
 
   def sentence?
-    
-  end
+    if  self.end_with?(".")
+     return true 
+   else 
+     false 
+  end 
+end 
+
+
 
   def question?
+    if self.end_with?("?")
+      return TRUE
+    else 
+      false 
+    end 
 
   end
+
+
+
 
   def exclamation?
-
+     if self.end_with?("!")
+       return true 
+    else 
+      false 
+      
+    end 
   end
+
+
 
   def count_sentences
+    self.split(/[!?.]\s/).count
+     
+   end
 
-  end
-end
+end 
+
